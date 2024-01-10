7629d556ebe3d281569d57e60a378cee9fe77bcc
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2024-01-10 09:02:12 +0100

    Ex.3.9, MoveFlix v. 2.6 updated favorite-movies code

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "../movie-card/movie-card.scss";
-import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from "../movie-card/movie-card_old";
 
-function FavoriteMovies({ favoriteMovieList, toggleFav, user, setUser, movies }) {
-  const [favorites, setFavorites] = useState(favoriteMovieList);
-
-  useEffect(() => {
-    setFavorites(favoriteMovieList);
-  }, [user, favoriteMovieList]);
+function FavoriteMovies({ favoriteMovieList, user, setUser }) {
   return (
     <Row>
       <Col xs={12}>
-       <h2 className="mt-5 text-center text-md-start">Favorite Movies</h2> 
+        <h2 className="mt-5 text-center text-md-start">Favorite Movies</h2>
       </Col>
-    <Row className="justify-content-center">
-        {
-        favorites?.length !== 0 ?
-        favorites?.map((movie) => (
-            <Col  xs={12} sm={6} md={4} lg={3} xl={2} className="mx-1 mt-2 mb-2" key={movie._id}>
-                <MovieCard
-                    user={user}
-                    setUser={setUser}
-                    movie={movie}
-                    movies={movies}
-                    isFavorite={user.FavoriteMovies.includes(movie._id)}
-                />
+      <Row className="justify-content-center">
+        {favoriteMovieList?.length !== 0 ? (
+          favoriteMovieList?.map((movie) => (
+            <Col
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              xl={2}
+              className="mx-1 mt-2 mb-2"
+              key={movie._id}
+            >
+              <MovieCard
+                user={user}
+                setUser={setUser}
+                movie={movie}
+                isFavorite={true}
+              />
             </Col>
-        ))
-        : <Col>
-        <p>There are no favorites Movies</p>
-        </Col>
-        }
+          ))
+        ) : (
+          <Col>
+            <p>There are no favorites Movies</p>
+          </Col>
+        )}
+      </Row>
     </Row>
-</Row>
-  )
+  );
 }
 
-export default FavoriteMovies
+export default FavoriteMovies;
---
+++ src/components/profile-view/favorite-movies_old.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Col } from "react-bootstrap";
+import "../movie-card/movie-card.scss";
+import { MovieCard } from '../movie-card/movie-card_old';
+
+function FavoriteMovies({ favoriteMovieList, user, setUser }) {
+
+  return (
+    <Row>
+      <Col xs={12}>
+       <h2 className="mt-5 text-center text-md-start">Favorite Movies</h2> 
+      </Col>
+    <Row className="justify-content-center">
+        {
+        favorites?.length !== 0 ?
+        favorites?.map((movie) => (
+            <Col  
+              xs={12} 
+              sm={6} 
+              md={4} 
+              lg={3} 
+              xl={2} 
+              className="mx-1 mt-2 mb-2" 
+              key={movie._id}
+            >
+                <MovieCard
+                    user={user}
+                    setUser={setUser}
+                    movie={movie}
+                    // movies={movies}
+                    isFavorite={user.FavoriteMovies.includes(movie._id)}
+                />
+            </Col>
+        ))
+      )  : (
+        <Col>
+          <p>There are no favorites Movies</p>
+        </Col>
+        )}
+    </Row>
+  </Row>
+  );
+}
+
+export default FavoriteMovies
